dd1503999a18c0aebab1d38b19ecbcebc2454628
AleX4270 <40200945+AleX4270@users.noreply.github.com>
2022-01-27 14:19:44 +0100

    stable brick controller

--- Assets/Scripts/Gameplay/BallMovement.cs
+++ Assets/Scripts/Gameplay/BallMovement.cs
@@ -8,6 +8,7 @@ public class BallMovement : MonoBehaviour
 {
     private float x, xDir;
     private Vector2 bounceDir;
+    private GameManager gm;
 
     [Range(0.1f, 1f)]
     public float bounceOffset;
@@ -19,6 +20,7 @@ public class BallMovement : MonoBehaviour
     private void Start()
     {
         ball = this.gameObject.GetComponent<Rigidbody2D>();
+        gm = FindObjectOfType<GameManager>();
     }
 
     private void OnCollisionEnter2D(Collision2D col)
@@ -35,6 +37,16 @@ public class BallMovement : MonoBehaviour
             ball.AddForce(new Vector2(bounceDir.x, 0.5f).normalized * this.speed * 100);
             Debug.Log("Kierunek: " + bounceDir.x);
             Debug.DrawLine(col.gameObject.transform.position, col.GetContact(0).point, Color.green);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        //If ball hit the brick
+        if(other.gameObject.CompareTag("Brick"))
+        {
+            Destroy(other.gameObject);
+            gm.addPoint();
         }
     }
 
--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,15 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private Touch touch;
+    private float playerActualScore;
 
     [Header("UI")] 
     public Button startBtn;
+    public TMP_Text playerScore;
 
     [Header("Ball")] 
     public Rigidbody2D ball;
@@ -18,12 +21,23 @@ public class GameManager : MonoBehaviour
     [Header("Player")] 
     public Rigidbody2D player;
     public PlayerMovement playerController;
-    
+
+    private void Start()
+    {
+        playerActualScore = 0;
+    }
+
     public void startGame()
     {
         ballController.freezeBall(false);
         playerController.freezePlayer(false);
         ballController.pushBall();
         startBtn.gameObject.SetActive(false);
+    }
+
+    public void addPoint()
+    {
+        playerActualScore++;
+        playerScore.text = playerActualScore.ToString();
     }
 }
